f8e48a2acec8856906c956c13cd6a7c51d327820
OneD23 <81880962+OneD23@users.noreply.github.com>
2026-04-11 18:02:03 -0400

    Improve factura/caja UI usability with keyboard flow and inline status

--- SistemaFerreteriaV8/Usuarios.cs
+++ SistemaFerreteriaV8/Usuarios.cs
@@ -1,4 +1,6 @@
 ﻿using SistemaFerreteriaV8.Clases;
+using SistemaFerreteriaV8.Domain.Security;
+using SistemaFerreteriaV8.Infrastructure.Security;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,16 +15,46 @@ namespace SistemaFerreteriaV8
 {
     public partial class Usuarios : Form
     {
+        private readonly Button _btnPermisosUsuario = new Button();
+        private readonly Button _btnAuditoria = new Button();
+
         public Usuarios()
         {
             InitializeComponent();
             SistemaFerreteriaV8.Clases.ThemeManager.ApplyToForm(this);
+            InicializarBotonPermisos();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void InicializarBotonPermisos()
         {
-            Form1 frm = (Form1)Application.OpenForms["Form1"];
-            if (Application.OpenForms.OfType<Form1>().Any())
+            _btnPermisosUsuario.Text = "Permisos Usuario";
+            _btnPermisosUsuario.Width = 150;
+            _btnPermisosUsuario.Height = 34;
+            _btnPermisosUsuario.Left = 12;
+            _btnPermisosUsuario.Top = Math.Max(button1.Bottom, button2.Bottom) + 12;
+            _btnPermisosUsuario.Click += async (_, _) => await AbrirPermisosUsuarioAsync();
+            Controls.Add(_btnPermisosUsuario);
+
+            _btnAuditoria.Text = "Auditoría";
+            _btnAuditoria.Width = 150;
+            _btnAuditoria.Height = 34;
+            _btnAuditoria.Left = _btnPermisosUsuario.Right + 12;
+            _btnAuditoria.Top = _btnPermisosUsuario.Top;
+            _btnAuditoria.Click += async (_, _) => await AbrirConsultaAuditoriaAsync();
+            Controls.Add(_btnAuditoria);
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            if (!await PermissionAccess.EnsurePermissionAsync(
+                    PermissionAccess.GetActiveEmployee(),
+                    AppPermissions.ClientesEditar,
+                    this,
+                    "editar clientes"))
+                return;
+
+            Form1 frm = (Form1)WinFormsApp.OpenForms["Form1"];
+            if (WinFormsApp.OpenForms.OfType<Form1>().Any())
             {
                 frm.AbrirFormulario(new VentanaCliente());
             }
@@ -30,15 +62,48 @@ namespace SistemaFerreteriaV8
             this.Dispose();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
-            Form1 frm = (Form1)Application.OpenForms["Form1"];
-            if (Application.OpenForms.OfType<Form1>().Any())
+            if (!await PermissionAccess.EnsurePermissionAsync(
+                    PermissionAccess.GetActiveEmployee(),
+                    AppPermissions.EmpleadosGestionar,
+                    this,
+                    "gestionar empleados"))
+                return;
+
+            Form1 frm = (Form1)WinFormsApp.OpenForms["Form1"];
+            if (WinFormsApp.OpenForms.OfType<Form1>().Any())
             {
                 frm.AbrirFormulario(new VentanaEmpleado());
             }
 
             this.Dispose();
         }
+        private async Task AbrirPermisosUsuarioAsync()
+        {
+            if (!await PermissionAccess.EnsurePermissionAsync(
+                    PermissionAccess.GetActiveEmployee(),
+                    AppPermissions.EmpleadosGestionar,
+                    this,
+                    "administrar permisos por usuario"))
+                return;
+
+            using var ventana = new VentanaPermisosUsuario();
+            ventana.ShowDialog(this);
+        }
+
+        private async Task AbrirConsultaAuditoriaAsync()
+        {
+            if (!await PermissionAccess.EnsurePermissionAsync(
+                    PermissionAccess.GetActiveEmployee(),
+                    AppPermissions.ReportesVer,
+                    this,
+                    "consultar auditoría operativa"))
+                return;
+
+            using var ventana = new VentanaAuditoriaConsulta();
+            ventana.ShowDialog(this);
+        }
+
     }
 }
